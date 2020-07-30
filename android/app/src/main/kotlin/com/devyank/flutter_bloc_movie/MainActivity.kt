b10105752cb204f000ea76eca51bdc1eb71c7800
Devyank Shaw <devyankshaw70@gmail.com>
2020-07-30 08:53:20 +0530

    Populating movies poster in movie list screen

---
+++ android/app/src/main/kotlin/com/devyank/flutter_bloc_movie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devyank.flutter_bloc_movie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_movie/src/app.dart';
+
+void main() {
+  runApp(App());
+}
